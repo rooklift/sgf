cf7632f59163edbbfd824d390495d10c57a0290c
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-17 11:43:03 +0100

    Create tree_utils.go

---
+++ tree_utils.go
@@ -0,0 +1,59 @@
+package sgf
+
+func (self *Node) RemoveChild(child *Node) {
+
+	if self == nil {
+		return
+	}
+
+	for i := len(self.Children) - 1; i >= 0; i-- {
+		if self.Children[i] == child {
+			self.Children = append(self.Children[:i], self.Children[i+1:]...)
+		}
+	}
+}
+
+func (self *Node) GetRoot() *Node {
+	node := self
+	for {
+		if node.Parent != nil {
+			node = node.Parent
+		} else {
+			return node
+		}
+	}
+}
+
+func (self *Node) GetEnd() *Node {
+	node := self
+	for {
+		if len(node.Children) > 0 {
+			node = node.Children[0]
+		} else {
+			return node
+		}
+	}
+}
+
+func (self *Node) GetLine() []*Node {		// The line of nodes from root to here
+
+	var ret []*Node
+
+	node := self
+
+	for {
+		ret = append(ret, node)
+		node = node.Parent
+		if node == nil {
+			break
+		}
+	}
+
+	// Reverse the slice...
+
+	for left, right := 0, len(ret) - 1; left < right; left, right = left + 1, right - 1 {
+		ret[left], ret[right] = ret[right], ret[left]
+	}
+
+	return ret
+}

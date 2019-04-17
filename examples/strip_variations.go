b6998b172050e1845237565d5dac78fa0128df1e
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-17 12:22:18 +0100

    Create strip_variations.go

---
+++ examples/strip_variations.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	sgf ".."
+)
+
+func main() {
+	root, err := sgf.Load(os.Args[1])
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	node := root
+
+	for {
+		for len(node.Children) > 1 {
+			node.RemoveChild(node.Children[len(node.Children) - 1])
+		}
+		node = node.MainChild()
+		if node == nil {
+			break
+		}
+	}
+
+	root.Save(os.Args[1])
+}

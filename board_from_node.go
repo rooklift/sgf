05e032743241d3c498376a750e38c26a1fae1e1a
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-18 16:40:11 +0100

    more exports

--- board_from_node.go
+++ board_from_node.go
@@ -10,11 +10,10 @@ func ClearBoardCache() { board_cache = make(map[*Node]*Board) }
 
 func (self *Node) Board() *Board {
 
+	// Returns a __COPY__ of the cached board for this node, creating that if needed.
+	//
 	// The cache relies on the fact that mutating properties B, W, AB, AW, AE cannot
 	// be added to a node after creation.
-	//
-	// Every return should be returning a copy, never the cached thing itself, so
-	// that the caller can safely manipulate its copy.
 
 	cached, ok := board_cache[self]
 
@@ -59,12 +58,12 @@ func (self *Board) update(node *Node) {
 	// Play move: B / W. Note that "moves" which are not valid onboard points are passes.
 
 	for _, p := range node.Props["B"] {
-		self.modify_with_move(p, BLACK)
+		self.PlaceStone(p, BLACK)
 		self.Player = WHITE
 	}
 
 	for _, p := range node.Props["W"] {
-		self.modify_with_move(p, WHITE)
+		self.PlaceStone(p, WHITE)
 		self.Player = BLACK
 	}
 
@@ -79,13 +78,13 @@ func (self *Board) update(node *Node) {
 	}
 }
 
-func (self *Board) modify_with_move(p string, colour Colour) {
+func (self *Board) PlaceStone(p string, colour Colour) {
 
 	// Other than sanity checks, there is no legality check here.
 	// Nor should there be.
 
 	if colour != BLACK && colour != WHITE {
-		panic("modify_with_move(): no colour")
+		panic("Board.PlaceStone(): no colour")
 	}
 
 	if Onboard(p, self.Size) == false {		// Consider this a pass
@@ -100,7 +99,7 @@ func (self *Board) modify_with_move(p string, colour Colour) {
 	for _, a := range AdjacentPoints(p, self.Size) {
 		if self.GetState(a) == opponent {
 			if self.HasLiberties(a) == false {
-				caps += self.destroy_group(a)
+				caps += self.DestroyGroup(a)
 			}
 		}
 	}
@@ -110,7 +109,7 @@ func (self *Board) modify_with_move(p string, colour Colour) {
 	// Handle suicide...
 
 	if self.HasLiberties(p) == false {
-		suicide_caps := self.destroy_group(p)
+		suicide_caps := self.DestroyGroup(p)
 		self.CapturesBy[opponent] += suicide_caps
 	}
 
@@ -129,7 +128,7 @@ func (self *Board) modify_with_move(p string, colour Colour) {
 	return
 }
 
-func (self *Board) destroy_group(p string) int {		// Returns stones removed.
+func (self *Board) DestroyGroup(p string) int {			// Returns stones removed.
 
 	colour := self.GetState(p)
 
@@ -143,7 +142,7 @@ func (self *Board) destroy_group(p string) int {		// Returns stones removed.
 	for _, a := range AdjacentPoints(p, self.Size) {
 
 		if self.GetState(a) == colour {
-			count += self.destroy_group(a)
+			count += self.DestroyGroup(a)
 		}
 	}
 

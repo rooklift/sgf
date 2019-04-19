2d0197a6feb751ecb2ab8adec92c6aa36e6766f0
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-19 22:44:15 +0100

    more impressive example

--- examples/mutate.go
+++ examples/mutate.go
@@ -1,8 +1,6 @@
 package main
 
-// Example of mutating an entire game tree.
-// The MutateTree() method is called with a function argument
-// which takes the current property map and returns the new one.
+// Example of using MutateTree() with a function argument. Rotates the whole tree.
 
 import (
 	"fmt"
@@ -22,19 +20,26 @@ func main() {
 		return
 	}
 
-	mutated := original.MutateTree(mirror_diagonal)
-
-	mutated.GetEnd().Board().DumpBoard()		// We could also save the new tree with mutated.Save()
+	mutated := original.MutateTree(rotate_clockwise)
+	original.GetEnd().Board().DumpBoard()				// Unharmed
 	fmt.Printf("\n")
-	original.GetEnd().Board().DumpBoard()		// Unharmed
-
+	mutated.GetEnd().Board().DumpBoard()				// We could also save with mutated.Save()
 }
 
-func mirror_diagonal(props map[string][]string, board *sgf.Board) map[string][]string {
+// This function will be called for every node in the original tree, with the properties and
+// the board position. It must return the properties it wants the mutated version to have.
+// It is safe for it to modify the "props" map it is supplied with.
+
+func rotate_clockwise(props map[string][]string, board *sgf.Board) map[string][]string {
 	for _, key := range []string{"AB", "AW", "AE", "B", "CR", "MA", "SL", "SQ", "TR", "W"} {
 		for i, s := range props[key] {
 			if len(s) == 2 {
-				props[key][i] = string(props[key][i][1]) + string(props[key][i][0])		// Diagonal mirror
+				x, y, onboard := sgf.XYFromSGF(s, board.Size)
+				if onboard {
+					new_x := board.Size - 1 - y
+					new_y := x
+					props[key][i] = sgf.Point(new_x, new_y)
+				}
 			}
 		}
 	}

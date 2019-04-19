90d05fb0b7c4ec2729ffac3c204cc7d60fe8af9f
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-19 19:48:41 +0100

    PlayMoveColour()

--- move.go
+++ move.go
@@ -4,32 +4,35 @@ import (
 	"fmt"
 )
 
-func (self *Node) PlayMove(p string) (*Node, error) {
+func (self *Node) PlayMoveColour(p string, colour Colour) (*Node, error) {
 
-	// Uses board info to determine colour.
-	// Returns the new node on success, or self on failure.
-
-	if self == nil { panic("Node.PlayMove(): called on nil node") }
+	if self == nil { panic("Node.PlayMoveColour(): called on nil node") }
 
 	board := self.Board()
 
 	x, y, onboard := XYFromSGF(p, board.Size)
 
 	if onboard == false {
-		return self, fmt.Errorf("Node.PlayMove(): invalid or off-board string \"%v\"", p)
+		return self, fmt.Errorf("Node.PlayMoveColour(): invalid or off-board string \"%v\"", p)
 	}
 
 	if board.GetState(p) != EMPTY {
-		return self, fmt.Errorf("Node.PlayMove(): point \"%v\" (%v,%v) was not empty", p, x, y)
+		return self, fmt.Errorf("Node.PlayMoveColour(): point \"%v\" (%v,%v) was not empty", p, x, y)
 	}
+
+	// FIXME? (Likely no.) Slight issue with ko square when a move of the the wrong colour is forced.
 
 	if board.HasKo() && board.Ko == p {
-		return self, fmt.Errorf("Node.PlayMove(): ko recapture forbidden")
+		return self, fmt.Errorf("Node.PlayMoveColour(): ko point forbidden")
 	}
 
-	key := "B"; if board.Player == WHITE { key = "W" }
+	if colour != BLACK && colour != WHITE {
+		return self, fmt.Errorf("Node.PlayMoveColour(): no colour specified")
+	}
 
 	// Return the already-extant child if there is such a thing...
+
+	key := "B"; if colour == WHITE { key = "W" }
 
 	for _, child := range self.Children {
 		mv, ok := child.GetValue(key)
@@ -45,10 +48,20 @@ func (self *Node) PlayMove(p string) (*Node, error) {
 
 	if proposed_board.GetState(p) == EMPTY {									// Because of suicide
 		self.RemoveChild(proposed_node)											// Delete child (see above)
-		return self, fmt.Errorf("Node.PlayMove(): suicide forbidden")
+		return self, fmt.Errorf("Node.PlayMoveColour(): suicide forbidden")
 	}
 
 	return proposed_node, nil
+}
+
+func (self *Node) PlayMove(p string) (*Node, error) {
+
+	// Uses board info to determine colour.
+	// Returns the new node on success, or self on failure.
+
+	if self == nil { panic("Node.PlayMove(): called on nil node") }
+
+	return self.PlayMoveColour(p, self.Board().Player)
 }
 
 func (self *Node) Pass() *Node {

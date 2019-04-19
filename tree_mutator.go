dbb443093eb1a09de107e73f55d9bdcfc65559af
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-19 21:36:15 +0100

    Create tree_mutator.go

---
+++ tree_mutator.go
@@ -0,0 +1,35 @@
+package sgf
+
+func (self *Node) MutateTree(mutator func(props map[string][]string)) *Node {
+	if self == nil { panic("Node.MutateTree(): called on nil node") }
+	root := self.GetRoot()
+	mutant_root := mutate_recursive(root, mutator)
+	return mutant_root
+}
+
+func mutate_recursive(node *Node, mutator func(props map[string][]string)) *Node {
+
+	mutant := make_mutant(node, mutator)
+
+	for _, child := range(node.Children) {
+		mutant_child := mutate_recursive(child, mutator)
+		mutant_child.Parent = mutant
+		mutant.Children = append(mutant.Children, mutant_child)
+	}
+
+	return mutant
+}
+
+func make_mutant(node *Node, mutator func(props map[string][]string)) *Node {
+
+	props := node.AllProperties()		// This returns a deep copy, so is safe to modify.
+
+	mutator(props)
+
+	// We call NewNode() with a nil parent so that we can handle parent/child relationships manually.
+	// We could in fact pass the parent as an argument to make_mutant() and so on but it is less clean.
+
+	mutant := NewNode(nil, props)
+
+	return mutant
+}

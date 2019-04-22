97258fede9863ffdd72bb18abc3c2b6fe308cdf9
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-23 00:18:25 +0100

    Update escaped.go

--- examples/escaped.go
+++ examples/escaped.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	node, _ := sgf.Load("escaped.sgf")
+	node, _ := sgf.Load("kifu/escaped.sgf")
 	node = node.GetEnd()
 	label, _ := node.GetValue("LB")
 	fmt.Printf("%v\n", label)

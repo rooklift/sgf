1a005b9168a221e8e628fbd3859c9305ef08a7d5
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-24 09:06:51 +0100

    remove the tree_mutator as pointless

--- examples/invert.go
+++ examples/invert.go
@@ -1,7 +1,5 @@
 package main
 
-// Example of using MutateTree() with a function argument. Flips colours.
-
 import (
 	"fmt"
 	"os"
@@ -10,50 +8,41 @@ import (
 	sgf ".."
 )
 
+var	reverse_map = map[string]string{
+	"B": "W", "W": "B", "AB": "AW", "AW": "AB", "PB": "PW", "PW": "PB"}
+
 func main() {
-	original := sgf.LoadArgOrQuit(1)					// Equivalent to sgf.Load(os.Args[1])
-	mutated := original.MutateTree(reverse_colours)
-	mutated.Save(os.Args[1] + ".inverted.sgf")
-	fmt.Printf("Saved. %d nodes in original, %d nodes in mutated.\n", original.TreeSize(), mutated.TreeSize())
-}
 
-// The mutator function is shown the original node and must return the properties
-// that it wants the mutated node to have...
+	root := sgf.LoadArgOrQuit(1)					// Equivalent to sgf.Load(os.Args[1])
+	nodes := root.TreeNodes()
 
-func reverse_colours(original *sgf.Node, boardsize int) *sgf.Node {
-
-	reverse_map := map[string]string{
-		"B": "W",
-		"W": "B",
-		"AB": "AW",
-		"AW": "AB",
-		"PB": "PW",
-		"PW": "PB",
-	}
-
-	node := original.Copy()
-
-	for old_key, new_key := range reverse_map {
-		node.SetValues(new_key, original.AllValues(old_key))
-	}
-
-	result, ok := node.GetValue("RE")
+	result, ok := root.GetValue("RE")
 	if ok {
 		if strings.HasPrefix(result, "B+") {
-			node.SetValue("RE", "W+" + result[2:])
+			root.SetValue("RE", "W+" + result[2:])
 		} else if strings.HasPrefix(result, "W+") {
-			node.SetValue("RE", "B+" + result[2:])
+			root.SetValue("RE", "B+" + result[2:])
 		}
 	}
 
-	komi, ok := node.GetValue("KM")
+	komi, ok := root.GetValue("KM")
 	if ok {
 		if strings.HasPrefix(komi, "-") {
-			node.SetValue("KM", komi[1:])
+			root.SetValue("KM", komi[1:])
 		} else {
-			node.SetValue("KM", "-" + komi)
+			root.SetValue("KM", "-" + komi)
 		}
 	}
 
-	return node
+	for _, node := range nodes {
+		dupe := node.Copy()
+		for old_key, new_key := range reverse_map {
+			node.SetValues(new_key, dupe.AllValues(old_key))
+		}
+	}
+
+	err := root.Save(os.Args[1] + ".inverted.sgf")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
 }
--- examples/rotate.go
+++ examples/rotate.go
@@ -1,7 +1,5 @@
 package main
 
-// Example of using MutateTree() with a function argument. Rotates the whole tree.
-
 import (
 	"fmt"
 	"os"
@@ -10,26 +8,26 @@ import (
 )
 
 func main() {
-	original := sgf.LoadArgOrQuit(1)					// Equivalent to sgf.Load(os.Args[1])
-	mutated := original.MutateTree(rotate_clockwise)
-	mutated.Save(os.Args[1] + ".rotated.sgf")
-	fmt.Printf("Saved. %d nodes in original, %d nodes in mutated.\n", original.TreeSize(), mutated.TreeSize())
-}
 
-// The mutator function is shown the original node and must return a new node with
-// no parent or children.
+	root := sgf.LoadArgOrQuit(1)					// Equivalent to sgf.Load(os.Args[1])
+	nodes := root.TreeNodes()
+	boardsize := root.RootBoardSize()
 
-func rotate_clockwise(original *sgf.Node, boardsize int) *sgf.Node {
-	node := original.Copy()
-	for _, key := range []string{"AB", "AW", "AE", "B", "CR", "MA", "SL", "SQ", "TR", "W"} {
-		all_values := node.AllValues(key)
-		for i, val := range all_values {
-			x, y, onboard := sgf.ParsePoint(val, boardsize)
-			if onboard {
-				all_values[i] = sgf.Point(boardsize - 1 - y, x)
+	for _, node := range nodes {
+		for _, key := range []string{"AB", "AW", "AE", "B", "CR", "MA", "SL", "SQ", "TR", "W"} {
+			all_values := node.AllValues(key)
+			for i, val := range all_values {
+				x, y, onboard := sgf.ParsePoint(val, boardsize)
+				if onboard {
+					all_values[i] = sgf.Point(boardsize - 1 - y, x)
+				}
 			}
+			node.SetValues(key, all_values)
 		}
-		node.SetValues(key, all_values)
 	}
-	return node
+
+	err := root.Save(os.Args[1] + ".rotated.sgf")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
 }

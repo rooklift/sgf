4989304b825f8cc55e4302fdc31b81f7299496ec
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-25 17:16:30 +0100

    Update handicap.go

--- examples/handicap.go
+++ examples/handicap.go
@@ -10,7 +10,7 @@ func main() {
 	for sz := 4; sz <= 19; sz++ {
 		for n := 2; n <= 9; n++ {
 			root := sgf.NewTree(sz)
-			for _, stone := range sgf.HandicapPoints(sz, n, true) {
+			for _, stone := range sgf.HandicapPoints(sz, n, false) {
 				root.AddValue("AB", stone)
 			}
 			root.Board().DumpBoard()

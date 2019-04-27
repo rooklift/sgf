8dbabc011cb17653e8dc70b7731c3ecc9165a874
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-04-28 00:35:59 +0100

    Update search.go

--- examples/search.go
+++ examples/search.go
@@ -14,7 +14,7 @@ import (
 func main() {
 
 	if len(os.Args) < 3 {
-		fmt.Printf("Usage: search.exe dirname playername")
+		fmt.Printf("Usage: %s dirname playername\n", filepath.Base(os.Args[0]))
 		return
 	}
 

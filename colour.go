1d2e52b7fb86128ec147cf6d16c125eba0856072
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-05-02 22:24:53 +0100

    Update colour.go

--- colour.go
+++ colour.go
@@ -11,28 +11,28 @@ const (
 // Opposite returns the opposite colour (if called on BLACK or WHITE) otherwise
 // it returns EMPTY.
 func (c Colour) Opposite() Colour {
-	if c == WHITE { return BLACK }
 	if c == BLACK { return WHITE }
+	if c == WHITE { return BLACK }
 	return EMPTY
 }
 
 // Upper returns a single byte string, "B" or "W" or "?", for the colour.
 func (c Colour) Upper() string {
-	if c == WHITE { return "W" }
 	if c == BLACK { return "B" }
+	if c == WHITE { return "W" }
 	return "?"
 }
 
 // Lower returns a single byte string, "b" or "w" or "?", for the colour.
 func (c Colour) Lower() string {
-	if c == WHITE { return "w" }
 	if c == BLACK { return "b" }
+	if c == WHITE { return "w" }
 	return "?"
 }
 
 // Word returns a word, "Black" or "White" or "??", for the colour.
 func (c Colour) Word() string {
-	if c == WHITE { return "White" }
 	if c == BLACK { return "Black" }
+	if c == WHITE { return "White" }
 	return "??"
 }

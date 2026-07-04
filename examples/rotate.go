ddca42365163c5f58fbffbcf1363c39e19202b95
rooklift <16438795+rooklift@users.noreply.github.com>
2026-07-04 13:27:50 +0100

    Update rotate.go

--- examples/rotate.go
+++ examples/rotate.go
@@ -1,6 +1,6 @@
 package main
 
-// Rotates a game tree. FIXME: for rectangular boards.
+// Rotates a game tree.
 
 import (
 	"fmt"
@@ -15,14 +15,13 @@ func main() {
 	nodes := root.TreeNodes()
 
 	width, height := root.RootBoardSize()
-	if width != height {
-		fmt.Printf("rotate: rectangular boards not supported\n")
-		os.Exit(1)
-	}
-	boardsize := width
 
 	for _, node := range nodes {
-		rotate(node, boardsize)
+		rotate(node, width, height)
+	}
+
+	if width != height {
+		root.SetValue("SZ", fmt.Sprintf("%v:%v", height, width))
 	}
 
 	err := root.Save(os.Args[1] + ".rotated.sgf")
@@ -31,13 +30,13 @@ func main() {
 	}
 }
 
-func rotate(node *sgf.Node, boardsize int) {
+func rotate(node *sgf.Node, width, height int) {
 	for _, key := range []string{"AB", "AW", "AE", "B", "CR", "MA", "SL", "SQ", "TR", "W"} {
 		all_values := node.AllValues(key)
 		for i, val := range all_values {
-			x, y, onboard := sgf.ParsePoint(val, boardsize, boardsize)
+			x, y, onboard := sgf.ParsePoint(val, width, height)
 			if onboard {
-				all_values[i] = sgf.Point(boardsize - 1 - y, x)
+				all_values[i] = sgf.Point(height - 1 - y, x)
 			}
 		}
 		node.SetValues(key, all_values)
